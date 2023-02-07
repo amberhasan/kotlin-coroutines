319a5ed8800ba5a8c0bcbcd24e2c16046ce6e46c
amberhasan <amber.hasan@gmail.com>
2023-02-07 13:29:44 -0600

    isActive is a way to tell if the job has been canceled; sometimes in intensive operations, the job does not check if it's been canceled in another coroutine

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -13,16 +13,25 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val job = GlobalScope.launch(Dispatchers.Default) {
-            repeat(5) {
-                Log.d(TAG, "Coroutine is still working...")
-                delay(1000L)
+            Log.d(TAG, "Starting long running calculation....")
+            for(i in 30..40) {
+                if(isActive){ //must check if the job has been canceled; otherwise this job will be so busy and not notice
+                    Log.d(TAG, "Result for i = $i: ${fib(i)}")
+                }
             }
+            Log.d(TAG, "Ending long running calculation...")
         }
 
         runBlocking {
             delay(2000L)
             job.cancel()
-            Log.d(TAG, "Main Thread is continuing...")
+            Log.d(TAG, "Canceled job...")
         }
+    }
+
+    fun fib(n: Int): Long {
+        return if(n==0) 0
+        else if(n==1) 1
+        else fib(n-1) + fib(n-2)
     }
 }
