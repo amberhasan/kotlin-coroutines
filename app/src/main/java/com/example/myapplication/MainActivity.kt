97f485b204b3704562a2afc6a0cdbaa34a26d695
amberhasan <amber.hasan@gmail.com>
2023-02-07 13:21:09 -0600

    join suspends the coroutine until the job is completed, so the whole job will repeat 5 times and finish and come back to the main thread

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -12,28 +12,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //You'd still be able to use the UI in the main thread. There would be no blocking.
-//        GlobalScope.launch(Dispatchers.Main) {
-//            delay(10000L)
-//        }
-
-        // Only affects main thread.
-        // This will block UI updates. Useful if you don't need coroutine behavior but would still like to call suspend function
-        //It's the same as if we called delay outside of this block (but you can't syntactically do that bc it needs to be in coroutine)
-        Log.d(TAG, "Before runBlocking")
-        runBlocking {//same output as if you did Thread.sleep(5000L)
-            launch (Dispatchers.IO) {//will run asynch
-                delay(3000L)
-                Log.d(TAG, "Finished IO Coroutine 1")
+        val job = GlobalScope.launch(Dispatchers.Default) {
+            repeat(5) {
+                Log.d(TAG, "Coroutine is still working...")
+                delay(1000L)
             }
-            launch (Dispatchers.IO) {//will run asynch
-                delay(3000L)
-                Log.d(TAG, "Finished IO Coroutine 2")
-            }
-            Log.d(TAG, "Start of runBlocking")
-            delay(5000L)
-            Log.d(TAG, "End of runBlocking")
         }
-        Log.d(TAG, "After runBlocking")
+
+        runBlocking {
+            job.join()
+            Log.d(TAG, "Main Thread is continuing..")
+        }
     }
 }
