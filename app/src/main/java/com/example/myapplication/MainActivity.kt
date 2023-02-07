cc46cc838d7b333e0bad2e1d2c646af08e1388cf
amberhasan <amber.hasan@gmail.com>
2023-02-07 13:06:41 -0600

    run blocking directly impacts main thread, not other threads

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,8 +3,6 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
@@ -14,20 +12,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        GlobalScope.launch(Dispatchers.IO) {
-            val answer = doNetworkCall()
-            Log.d(TAG, "Starting coroutine in thread ${Thread.currentThread().name}")
+        //You'd still be able to use the UI in the main thread. There would be no blocking.
+//        GlobalScope.launch(Dispatchers.Main) {
+//            delay(10000L)
+//        }
 
-            //this code will now be executed in the main thread
-            withContext(Dispatchers.Main) {
-                Log.d(TAG, "Setting text in thread ${Thread.currentThread().name}")
-                //Can update UI here with network call info, with main thread
+        // Only affects main thread.
+        // This will block UI updates. Useful if you don't need coroutine behavior but would still like to call suspend function
+        //It's the same as if we called delay outside of this block (but you can't syntactically do that bc it needs to be in coroutine)
+        Log.d(TAG, "Before runBlocking")
+        runBlocking {//same output as if you did Thread.sleep(5000L)
+            launch (Dispatchers.IO) {//will run asynch
+                delay(3000L)
+                Log.d(TAG, "Finished IO Coroutine 1")
             }
+            launch (Dispatchers.IO) {//will run asynch
+                delay(3000L)
+                Log.d(TAG, "Finished IO Coroutine 2")
+            }
+            Log.d(TAG, "Start of runBlocking")
+            delay(5000L)
+            Log.d(TAG, "End of runBlocking")
         }
-    }
-
-    private suspend fun doNetworkCall(): String {
-        delay(3000L)
-        return "This is the answer"
+        Log.d(TAG, "After runBlocking")
     }
 }
